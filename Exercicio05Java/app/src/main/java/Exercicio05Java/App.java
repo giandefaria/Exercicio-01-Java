4cfbd39635719de4d47122b76c465822bdf26049
Gianbdf <gian.defaria@hotmail.com>
2023-03-27 12:22:52 -0300

    arquivos iniciais

---
+++ Exercicio05Java/app/src/main/java/Exercicio05Java/App.java
@@ -0,0 +1,14 @@
+/*
+Escreva um algoritmo que leia o nome de um aluno e as notas das três provas que ele obteve no semestre.
+No finalinformar o nome do aluno e a sua média (aritmética);
+ */
+package Exercicio05Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        
+    }
+}
