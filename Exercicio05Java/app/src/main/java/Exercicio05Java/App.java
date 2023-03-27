07a8948ee9bd0a7482bb05503ac316413c6a0623
Gianbdf <gian.defaria@hotmail.com>
2023-03-27 12:25:30 -0300

    adicionando variáveis de entrada e importando o scanner

--- Exercicio05Java/app/src/main/java/Exercicio05Java/App.java
+++ Exercicio05Java/app/src/main/java/Exercicio05Java/App.java
@@ -4,11 +4,18 @@ No finalinformar o nome do aluno e a sua média (aritmética);
  */
 package Exercicio05Java;
 
+import java.util.Scanner;
+
 public class App {
 
 
     public static void main(String[] args) {
-
+        String nomeAluno;
+        float notaUm;
+        float notaDois;
+        float notaTres;
+        
+        Scanner leitor = new Scanner(System.in);
         
     }
 }
