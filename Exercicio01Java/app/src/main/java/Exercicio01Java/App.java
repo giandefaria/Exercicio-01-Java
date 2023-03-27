65d919366824073b8baa4036f84effe112361767
Gianbdf <gian.defaria@hotmail.com>
2023-03-27 01:31:39 -0300

    criado um algoritmo que pede dois número do usuário e mostra a soma ao final

--- Exercicio01Java/app/src/main/java/Exercicio01Java/App.java
+++ Exercicio01Java/app/src/main/java/Exercicio01Java/App.java
@@ -6,7 +6,18 @@ public class App {
 
     public static void main(String[] args) {
         
+        int primeiroNumero = 0;
+        int segundoNumero = 0;
+        
         Scanner leitor = new Scanner(System.in);
         
+        System.out.println("Digite o primeiro número");
+        primeiroNumero = leitor.nextInt();
+        
+        System.out.println("Digite o segundo número");
+        segundoNumero = leitor.nextInt();
+        
+        System.out.println("O resultado da soma dos dois número é: " +
+                (primeiroNumero + segundoNumero));
     }
 }
