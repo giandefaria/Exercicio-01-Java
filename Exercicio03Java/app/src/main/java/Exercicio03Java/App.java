c8facdffd8313e5a598d3be346aadd23e28efb81
Gianbdf <gian.defaria@hotmail.com>
2023-03-27 01:56:34 -0300

    arquivos iniciais

---
+++ Exercicio03Java/app/src/main/java/Exercicio03Java/App.java
@@ -0,0 +1,13 @@
+/*
+Escreva um algoritmo para determinar o consumo médio de um automóvel sendo fornecida a distância
+total percorrida pelo automóvel e o total de combustível gasto;
+*/
+package Exercicio03Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+    }
+}
