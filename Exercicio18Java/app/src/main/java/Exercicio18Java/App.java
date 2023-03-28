eb9dbfe0add79e2e3f46b426efce0fb7b5f4fee5
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 20:56:08 -0300

    verificando se a pessoa é maior ou  menor de idade de acordo com a idade informada

--- Exercicio18Java/app/src/main/java/Exercicio18Java/App.java
+++ Exercicio18Java/app/src/main/java/Exercicio18Java/App.java
@@ -8,17 +8,24 @@ import java.util.Scanner;
 
 public class App {
 
-
     public static void main(String[] args) {
-        
+
         int idade;
         int quantidadeRepeticoes = 5;
-        
+
         Scanner leitor = new Scanner(System.in);
-        
+
         while (quantidadeRepeticoes > 0) {
-        
+            System.out.println("Informe a idade da pessoa:");
+            idade = leitor.nextInt();
+
+            if (idade >= 18) {
+                System.out.println("É maior de idade");
+            } else {
+                System.out.println("É menor de idade");
+            }
+
+            quantidadeRepeticoes--;
         }
-        
     }
 }
