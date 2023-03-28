d075e5f4236d15a5a3b838c2e0520d0dac3d8455
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 01:24:22 -0300

    adicionando valores de entrada

--- Exercicio14Java/app/src/main/java/Exercicio14Java/App.java
+++ Exercicio14Java/app/src/main/java/Exercicio14Java/App.java
@@ -3,9 +3,16 @@
  */
 package Exercicio14Java;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-
+        
+        int valorA;
+        int valorB;
+        
+        Scanner leitor = new Scanner(System.in);
+        
     }
 }
