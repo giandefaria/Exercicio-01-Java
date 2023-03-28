894d29ccd221c331e5fb10ab842007b83b060181
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 01:23:27 -0300

    arquivos iniciais exercício 14

---
+++ Exercicio14Java/app/src/main/java/Exercicio14Java/App.java
@@ -0,0 +1,11 @@
+/*
+ Escreva um algoritmo que leia dois valores inteiro distintos e informe qual é o maior;
+ */
+package Exercicio14Java;
+
+public class App {
+
+    public static void main(String[] args) {
+
+    }
+}
