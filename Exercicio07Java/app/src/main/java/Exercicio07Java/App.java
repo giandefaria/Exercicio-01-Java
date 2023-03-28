7a7b63a360cab01b83b3661ecf374fe3110c37de
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:23:16 -0300

    arquivos iniciais exercicio 7

---
+++ Exercicio07Java/app/src/main/java/Exercicio07Java/App.java
@@ -0,0 +1,15 @@
+/*
+ Leia uma temperatura em graus Celsius e apresentá-la convertida em graus Fahrenheit. Afórmula de
+conversão é:F=(9*C+160) / 5, sendo F a temperatura em Fahrenheit e C a temperatura emCelsius;
+ */
+package Exercicio07Java;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
