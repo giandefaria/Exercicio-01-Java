e93fa0b79ae29df806e75bac94e5478829bb5073
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 12:44:06 -0300

    finalizando algoritmo que verificará se o número informado está entre 100 e 200

--- Exercicio15Java/app/src/main/java/Exercicio15Java/App.java
+++ Exercicio15Java/app/src/main/java/Exercicio15Java/App.java
@@ -13,6 +13,14 @@ public class App {
         
         Scanner leitor = new Scanner(System.in);
         
+        System.out.println("Digite um número");
+        numero = leitor.nextInt();
         
+        if (numero >= 100 && numero <= 200) {
+            
+            System.out.println("O número informado está no intervalo entre 100 e 200");   
+        } else {
+            System.out.println("O número informado não está no intervalo entre 100 e 200");
+        }
     }
 }
