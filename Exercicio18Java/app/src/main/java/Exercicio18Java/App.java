2f38f0c5c9b695db4980edd79e57997ed964c198
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 20:50:42 -0300

    arquivos iniciais

---
+++ Exercicio18Java/app/src/main/java/Exercicio18Java/App.java
@@ -0,0 +1,13 @@
+/*
+Faça um algoritmo que receba a idade de 75 pessoas e mostre mensagem informando “maior de idade” e
+“menor de idade” para cada pessoa. Considere a idade a partir de 18 anos como maior de idade;
+ */
+package Exercicio18Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
