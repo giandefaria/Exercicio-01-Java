2cf722542ce47953ced3fa3dd2ae5c830de56ecc
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 01:21:14 -0300

    finalizado algoritmo do exercicio 13, o qual era simples, pois bastava utilizar if ou else

--- Exercicio13Java/app/src/main/java/Exercicio13Java/App.java
+++ Exercicio13Java/app/src/main/java/Exercicio13Java/App.java
@@ -10,6 +10,17 @@ public class App {
     public static void main(String[] args) {
         
         int numero;
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        System.out.println("Informe um número");
+        numero = leitor.nextInt();
+        
+        if (numero > 10) {
+            System.out.println("Número informado é maior que 10");
+        } else {
+            System.out.println("Número informado é menor ou igual a 10");
+        }
 
     }
 }
