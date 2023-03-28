f09ada5fbb2c54b342c869f22ba267be90b9f8f8
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:41:58 -0300

    arquivos iniciais exercicio 9

---
+++ Exercicio09Java/app/src/main/java/Exercicio09Java/App.java
@@ -0,0 +1,14 @@
+/*
+Faça um algoritmo que receba um valor que foi depositado e exiba o valor com rendimento após um
+mês.Considere fixo o juro da poupança em 0,07% a. m
+ */
+package Exercicio09Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+
+        
+    }
+}
