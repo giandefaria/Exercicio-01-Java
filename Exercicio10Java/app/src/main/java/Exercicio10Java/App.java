e254a711f1f9763871ed419a16b47a45bf06982a
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:51:58 -0300

    arquivos iniciais exercicio 10

---
+++ Exercicio10Java/app/src/main/java/Exercicio10Java/App.java
@@ -0,0 +1,13 @@
+/*
+A Loja Mamão com Açúcar está vendendo seus produtos em5(cinco)prestações sem juros. Faça um
+algoritmo que receba um valor de uma compra e mostre o valor das prestações;
+ */
+package Exercicio10Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
