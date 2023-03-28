3e9cefa39dcb99a8c16b6fb92aa5cfac7538bf17
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 20:58:09 -0300

    arquivos iniciais do exercicio 19

---
+++ Exercicio19Java/app/src/main/java/Exercicio19Java/App.java
@@ -0,0 +1,13 @@
+/*
+Escreva um algoritmo que leia o nome e o sexo de 56 pessoas e informe o nome e se ela é homem ou
+mulher. No final informe total de homens e de mulheres
+ */
+package Exercicio19Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
