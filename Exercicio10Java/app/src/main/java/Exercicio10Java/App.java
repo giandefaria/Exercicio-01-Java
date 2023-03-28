89262b753534e4cdc85dc7333e95568c13ac5a6a
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:53:24 -0300

    adicionando valores de entrada do algoritmo

--- Exercicio10Java/app/src/main/java/Exercicio10Java/App.java
+++ Exercicio10Java/app/src/main/java/Exercicio10Java/App.java
@@ -1,13 +1,18 @@
 /*
-A Loja Mamão com Açúcar está vendendo seus produtos em5(cinco)prestações sem juros. Faça um
+A Loja Mamão com Açúcar está vendendo seus produtos em 5(cinco)prestações sem juros. Faça um
 algoritmo que receba um valor de uma compra e mostre o valor das prestações;
  */
 package Exercicio10Java;
 
+import java.util.Scanner;
+
 public class App {
 
-
     public static void main(String[] args) {
+        
+        float valorProduto;
+        
+        System.out.println("Digite o valor do produto: ");
         
     }
 }
