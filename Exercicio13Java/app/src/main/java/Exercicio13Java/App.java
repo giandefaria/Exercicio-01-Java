16520dd846491bdfbe5eeb6b713c67574ad29ba4
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 01:17:42 -0300

    arquivos iniciais exercicio 13

---
+++ Exercicio13Java/app/src/main/java/Exercicio13Java/App.java
@@ -0,0 +1,13 @@
+/*
+ Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 10;
+ */
+package Exercicio13Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        
+
+    }
+}
