b149e25bfdc1da8d35b70d34f5bd4e811c553265
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:31:07 -0300

    arquivos iniciais exercicio 8

---
+++ Exercicio08Java/app/src/main/java/Exercicio08Java/App.java
@@ -0,0 +1,15 @@
+/*
+Elabore um algoritmo que efetue a apresentação do valor da conversão em real (R$) de um valorlido em
+dólar (US$). O algoritmo deverá solicitar o valor da cotação do dólar e também aquantidade de dólares
+disponíveis com ousuário;
+ */
+package Exercicio08Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+
+    }
+}
