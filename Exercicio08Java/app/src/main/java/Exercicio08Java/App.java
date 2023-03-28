0a492cfd3c02c0614765595550d378587ad946e1
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 00:32:43 -0300

    adicionando as variáveis

--- Exercicio08Java/app/src/main/java/Exercicio08Java/App.java
+++ Exercicio08Java/app/src/main/java/Exercicio08Java/App.java
@@ -1,5 +1,5 @@
 /*
-Elabore um algoritmo que efetue a apresentação do valor da conversão em real (R$) de um valorlido em
+Elabore um algoritmo que efetue a apresentação do valor da conversão em real (R$) de um valor lido em
 dólar (US$). O algoritmo deverá solicitar o valor da cotação do dólar e também aquantidade de dólares
 disponíveis com ousuário;
  */
@@ -9,6 +9,9 @@ public class App {
 
 
     public static void main(String[] args) {
+        float valorEmDolar = 0f;
+        float cotacaoDoDolar = 0f;
+        float conversaoEmReal = 0f;
         
 
     }
