a8c7ed59bb023e3edfea689b63c4f7d893d8cc6b
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 12:55:16 -0300

    arquivos iniciais exercicio 17

---
+++ Exercicio17Java/app/src/main/java/Exercicio17Java/App.java
@@ -0,0 +1,13 @@
+/*
+Leia 80 números e ao final informar quantos número(s) est(á)ão no intervalo entre 10 (inclusive) e 150
+(inclusive);
+ */
+package Exercicio17Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
