b7de6011021a7d1ed9485f7965964532629ccf37
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 12:50:53 -0300

    arquivos iniciais exercicio 25

---
+++ Exercicio25Java/app/src/main/java/Exercicio25Java/App.java
@@ -0,0 +1,13 @@
+/*
+Faça um algoritmo que leia dois números e identifique se são iguais ou diferentes. Caso eles sejam iguais
+imprima uma mensagem dizendo que eles são iguais. Caso sejam diferentes, informe qual número é o
+maior, e uma mensagem que são diferentes
+ */
+package Exercicio25Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
