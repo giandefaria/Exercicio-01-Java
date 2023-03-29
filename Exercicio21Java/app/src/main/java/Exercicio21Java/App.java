81cf3e2ddb968a36461063343baac331fd86a193
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 21:56:44 -0300

    arquivos iniciais exercicio 21

---
+++ Exercicio21Java/app/src/main/java/Exercicio21Java/App.java
@@ -0,0 +1,12 @@
+/*
+Escreva um algoritmo que leia os dados de “N” pessoas (nome, sexo, idade e saúde) e informe se está apta
+ou não para cumprir o serviço militar obrigatório. Informe os totais;
+ */
+package Exercicio21Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
