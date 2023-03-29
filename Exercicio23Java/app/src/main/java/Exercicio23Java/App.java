ba1f96007ee51cc0a84a2fa9a9da8d8bbf9da2ed
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 12:40:27 -0300

    finalizando algoritmo exercicio 23

--- Exercicio23Java/app/src/main/java/Exercicio23Java/App.java
+++ Exercicio23Java/app/src/main/java/Exercicio23Java/App.java
@@ -8,26 +8,24 @@ import java.util.Scanner;
 
 public class App {
 
-
     public static void main(String[] args) {
-        
+
         int numero;
-        
+
         Scanner leitor = new Scanner(System.in);
-        
+
         System.out.println("Informe um número:");
         numero = leitor.nextInt();
-        
+
         if (numero > 80) {
             System.out.println("O número informado é maior que 80");
-        } else if(numero == 40 ) {
+        } else if (numero == 40) {
             System.out.println("O número é igual a 40");
         } else if (numero < 25) {
             System.out.println("O número é menor que 25");
         } else {
             System.out.println("O numero está entre 25 e 80 e não é 40");
         }
-        
-        
+
     }
 }
