3759e323d64d6f01c92e006c419486aa212975ce
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 12:52:56 -0300

    adicionando atributos de entrada

--- Exercicio25Java/app/src/main/java/Exercicio25Java/App.java
+++ Exercicio25Java/app/src/main/java/Exercicio25Java/App.java
@@ -1,13 +1,20 @@
 /*
 Faça um algoritmo que leia dois números e identifique se são iguais ou diferentes. Caso eles sejam iguais
 imprima uma mensagem dizendo que eles são iguais. Caso sejam diferentes, informe qual número é o
-maior, e uma mensagem que são diferentes
+maior, e uma mensagem que são diferentes;
  */
 package Exercicio25Java;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        
+        int numeroUm;
+        int numeroDois;
+        
+        Scanner leitor = new Scanner(System.in);
         
     }
 }
