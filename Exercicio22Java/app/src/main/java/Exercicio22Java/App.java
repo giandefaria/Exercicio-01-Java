2fbaacfc1ce8f2884740a24988f3e4b33062ef3d
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 00:13:04 -0300

    adicionando dados de entrada iniciais

--- Exercicio22Java/app/src/main/java/Exercicio22Java/App.java
+++ Exercicio22Java/app/src/main/java/Exercicio22Java/App.java
@@ -1,14 +1,21 @@
 /*
 Faça um algoritmo que receba o preço de custo e o preço de venda de 40 produtos. Mostre como resultado
 se houve lucro, prejuízo ou empate para cada produto. Informe o valor de custo de cada produto, o valor
-de venda de cada produto, amédia de preço de custo e do preço de venda;
+de venda de cada produto, a média de preço de custo e do preço de venda;
  */
 package Exercicio22Java;
+
+import java.util.Scanner;
 
 public class App {
 
 
     public static void main(String[] args) {
+        
+        float precoCusto;
+        float precoVenda;
+    
+        
         
     }
 }
