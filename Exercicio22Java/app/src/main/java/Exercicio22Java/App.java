df45d32009b431e89d121001654bc1548232ba7c
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 00:24:33 -0300

    adicionando código para guardar valor total do preço de custo e de venda, para depois descobrir a média

--- Exercicio22Java/app/src/main/java/Exercicio22Java/App.java
+++ Exercicio22Java/app/src/main/java/Exercicio22Java/App.java
@@ -12,10 +12,31 @@ public class App {
 
     public static void main(String[] args) {
         
-        float precoCusto;
-        float precoVenda;
+        float precoCusto = 0f;
+        float precoVenda = 0f;
+        float precoCustoTotal = 0f;
+        float precoVendaTotal = 0f;
     
+        Scanner leitor = new Scanner(System.in);
         
+        int i = 1;
+        
+        for (; i <= 5; i++) {
+            System.out.println("Informe o preço de custo:");
+            precoCusto = leitor.nextFloat();
+            
+            System.out.println("Informe o preço de venda:");
+            precoVenda = leitor.nextFloat();
+            
+            precoCustoTotal = precoCusto + precoCusto;
+            precoVendaTotal = precoVenda + precoVenda;
+            
+            System.out.println("O preço do produto é de R$ " + precoCusto + ", e foi vendido por R$" + precoVenda);
+            
+        }
+        
+        System.out.println("A media do preço de custo foi de R$ " + (precoCustoTotal / i));
+        System.out.println("A media do preço de venda foi de R$ " + (precoVendaTotal / i));
         
     }
 }
