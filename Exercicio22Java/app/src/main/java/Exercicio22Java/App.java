2da5c1853bce6cc19ba70c9111d5255f4e79097b
Gianbdf <gian.defaria@hotmail.com>
2023-03-28 22:42:10 -0300

    arquivos iniciais do exercicio 22

---
+++ Exercicio22Java/app/src/main/java/Exercicio22Java/App.java
@@ -0,0 +1,14 @@
+/*
+Faça um algoritmo que receba o preço de custo e o preço de venda de 40 produtos. Mostre como resultado
+se houve lucro, prejuízo ou empate para cada produto. Informe o valor de custo de cada produto, o valor
+de venda de cada produto, amédia de preço de custo e do preço de venda;
+ */
+package Exercicio22Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
