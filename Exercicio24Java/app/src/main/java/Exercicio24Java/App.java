81625e316c26e5c57d9d1ecafbd26b6bbc709755
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 12:42:47 -0300

    arquivos iniciais exercicio 24

---
+++ Exercicio24Java/app/src/main/java/Exercicio24Java/App.java
@@ -0,0 +1,11 @@
+/*
+Faça um algoritmo que receba “N” números e mostre positivo, negativo ou zero para cada número;
+ */
+package Exercicio24Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
