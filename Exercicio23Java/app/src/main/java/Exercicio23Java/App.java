c5e98fa7c184a2bf8673324b27bd95ce5620a962
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 12:32:58 -0300

    adicionando arquivos iniciais

--- Exercicio23Java/app/src/main/java/Exercicio23Java/App.java
+++ Exercicio23Java/app/src/main/java/Exercicio23Java/App.java
@@ -1,13 +1,17 @@
 /*
-Faça um algoritmo que receba um número e mostre uma mensagem caso este número sege maior que 80,
+Faça um algoritmo que receba um número e mostre uma mensagem caso este número seja maior que 80,
 menor que 25 ou igual a 40;
  */
 package Exercicio23Java;
+
+import java.util.Scanner;
 
 public class App {
 
 
     public static void main(String[] args) {
+        
+        int numero;
         
     }
 }
