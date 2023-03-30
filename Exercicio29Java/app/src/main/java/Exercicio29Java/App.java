24d3cb64f0bcf8cf78b474f3ff8fd2a0695cef8c
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 13:08:31 -0300

    arquivos iniciais e primeiras variáeis de entrada

---
+++ Exercicio29Java/app/src/main/java/Exercicio29Java/App.java
@@ -0,0 +1,18 @@
+/*
+Faça um algoritmo que receba o número do mês e mostre o mês correspondente. Valide mês inválido;
+ */
+package Exercicio29Java;
+
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int mes;
+        
+        Scanner leitor = new Scanner(System.in);
+        
+        
+    }
+}
