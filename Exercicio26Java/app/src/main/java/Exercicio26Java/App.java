f2aadf436bcac7e7e6d07bef2cd23516e2222efa
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 21:12:38 -0300

    informando por extenso o número de 1 a 5 selecionado.

--- Exercicio26Java/app/src/main/java/Exercicio26Java/App.java
+++ Exercicio26Java/app/src/main/java/Exercicio26Java/App.java
@@ -18,7 +18,20 @@ public class App {
         for(int i = 0; i < 5; i++) {
             System.out.println("Informe um número entre 1 a 5");
             numero = leitor.nextInt();
-        
+            
+            if (numero == 1) {
+                System.out.println("Número um");
+            } else if(numero == 2) {
+                System.out.println("Número dois");
+            } else if(numero == 3) {
+                System.out.println("Número três");
+            } else if(numero == 4) {
+                System.out.println("Número quatro");
+            } else if (numero == 5) {
+                System.out.println("Número cinco");
+            } else {
+                System.out.println("Número inválido");
+            }
         }
     }
 }
