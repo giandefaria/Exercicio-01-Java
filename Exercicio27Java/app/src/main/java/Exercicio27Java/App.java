92a7c60cd97d1a612fa607e022dd91ab9880ce33
Gianbdf <gian.defaria@hotmail.com>
2023-03-29 21:14:30 -0300

    arquivos iniciais exercicio 27

---
+++ Exercicio27Java/app/src/main/java/Exercicio27Java/App.java
@@ -0,0 +1,16 @@
+/*
+A concessionária de veículos “CARANGO” está vendendo os seus veículos com desconto. Faça um
+algoritmo que calcule e exiba o valor do desconto e o valor a ser pago pelo cliente. O desconto deverá ser
+calculado sobre o valor do veículo de acordo com o combustível (álcool – 25%, gasolina – 21% ou diesel
+–14%). Com valor do veículo zero encerra entrada de dados. Informe total de desconto e total pago pelos
+clientes
+ */
+package Exercicio27Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
