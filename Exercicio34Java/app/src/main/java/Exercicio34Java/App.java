98ef78b178e4757ae762fd868e768dcbba147dbe
Gianbdf <gian.defaria@hotmail.com>
2023-03-31 01:04:52 -0300

    arquivos iniciais

---
+++ Exercicio34Java/app/src/main/java/Exercicio34Java/App.java
@@ -0,0 +1,17 @@
+/*
+Elabore um algoritmo que, dada a idade de um nadador. Classifique-o em uma das seguintes categorias:
+a. Infantil A = 5 - 7 anos;
+b. Infantil B = 8 - 10 anos;
+c. Juvenil A = 11- 13 anos;
+d. Juvenil B = 14 - 17 anos;
+e. Sênior = 18 - 25 anos.
+Apresentar mensagem “idade fora da faixa etária” quando for outro ano não contemplado;
+ */
+package Exercicio34Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
