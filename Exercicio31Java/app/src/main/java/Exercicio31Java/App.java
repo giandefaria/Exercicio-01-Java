4ed2d8e48602868492c62fb6ad0b2071785c46fc
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 21:56:20 -0300

    arquivos iniciais

---
+++ Exercicio31Java/app/src/main/java/Exercicio31Java/App.java
@@ -0,0 +1,13 @@
+/*
+Dados três valores A, B e C, em que A e B são números reais e C é um caractere, pede-se para imprimir o
+resultado da operação de A por B se C for um símbolo de operador aritmético; caso contrário deve ser
+impressa uma mensagem de operador não definido. Tratar erro de divisão por zero;
+ */
+package Exercicio31Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
