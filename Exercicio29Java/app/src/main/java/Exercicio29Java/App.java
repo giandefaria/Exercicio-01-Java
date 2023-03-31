e7310c2960340d6e525d87f62311a911ad1563db
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 21:21:31 -0300

    adicionando o método de repetição while e completando os 12 meses do calendário

--- Exercicio29Java/app/src/main/java/Exercicio29Java/App.java
+++ Exercicio29Java/app/src/main/java/Exercicio29Java/App.java
@@ -10,27 +10,33 @@ public class App {
     public static void main(String[] args) {
         
         int mes;
+        char desejaRepetir = 's';
         
         Scanner leitor = new Scanner(System.in);
         
-        System.out.println("Digite o número do mês de 1 a 12");
-        mes = leitor.nextInt();
+        while (desejaRepetir == 's' || desejaRepetir == 'S') {
+            System.out.println("Digite o número do mês de 1 a 12");
+            mes = leitor.nextInt();
+
+            switch (mes) {
+                case 1 -> System.out.println("Mês de janeiro");
+                case 2 -> System.out.println("Mês de fevereiro");
+                case 3 -> System.out.println("Mês de março");
+                case 4 -> System.out.println("Mês de abril");
+                case 5 -> System.out.println("Mês de maio");
+                case 6 -> System.out.println("Mês de junho");
+                case 7 -> System.out.println("Mês de julho");
+                case 8 -> System.out.println("Mês de agosto");
+                case 9 -> System.out.println("Mês de setembro");
+                case 10 -> System.out.println("Mês de outubro");
+                case 11 -> System.out.println("Mês de novembro");
+                case 12 -> System.out.println("Mês de dezembro");
+                default -> System.out.println("Mês não existente. Digite valor de 1 a 12");
+            }
         
-        switch (mes) {
-            case 1 -> System.out.println("Mês de janeiro");
-            case 2 -> System.out.println("Mês de fevereiro");
-            case 3 -> System.out.println("Mês de março");
-            case 4 -> System.out.println("Mês de abril");
-            case 5 -> System.out.println("Mês de maio");
-            case 6 -> System.out.println("Mês de junho");
-            case 7 -> System.out.println("Mês de julho");
-            case 8 -> System.out.println("Mês de agosto");
-            case 9 -> System.out.println("Mês de setembro");
-            case 10 -> System.out.println("Mês de outubro");
-            case 11 -> System.out.println("Mês de novembro");
-            case 12 -> System.out.println("Mês de dezembro");
-            default -> System.out.println("Mês não existente. Digite valor de 1 a 12");
-        }
-        
+            System.out.println("Deseja continuar? S - Sim, N - Não");
+            desejaRepetir = leitor.next().charAt(0);
+        }    
+                
     }
 }
