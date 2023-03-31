5377d946330ee15ad8dd45f66e9098490fda54de
Gianbdf <gian.defaria@hotmail.com>
2023-03-31 01:15:03 -0300

    exercicio 35 arquivos iniciais

---
+++ Exercicio35Java/app/src/main/java/Exercicio35Java/App.java
@@ -0,0 +1,16 @@
+/*
+Faça um algoritmo que calcule o valor da conta de luz de uma pessoa. Sabe-se que o cálculo da conta de
+luz segue a tabela abaixo:
+Tipo de Cliente Valor do KW/h
+a. (Residência) 0,60;
+b. (Comércio) 0,48;
+c. (Indústria) 1,29
+ */
+package Exercicio35Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
