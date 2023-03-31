caa1b37f022395d1605523ed4a6a7499ec6532a5
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 21:24:16 -0300

    arquivos iniciais

---
+++ Exercicio30Java/app/src/main/java/Exercicio30Java/App.java
@@ -0,0 +1,12 @@
+/*
+Escreva um algoritmo que leia três valores inteiros distintos e os escreva em ordem crescente;
+ */
+package Exercicio30Java;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+    }
+}
