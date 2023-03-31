f488682c03a25388b696d9b4589057bcc8361045
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 21:13:53 -0300

    valores de entrada. Utilizarei o método switch

--- Exercicio29Java/app/src/main/java/Exercicio29Java/App.java
+++ Exercicio29Java/app/src/main/java/Exercicio29Java/App.java
@@ -13,6 +13,16 @@ public class App {
         
         Scanner leitor = new Scanner(System.in);
         
+        System.out.println("Digite o número do mês de 1 a 12");
+        mes = leitor.nextInt();
+        
+        switch (mes) {
+            case 1:
+                System.out.println("Mês de janeiro");
+                break;
+            default:
+                System.out.println("Mês não existente. Digite valor de 1 a 12");
+        }
         
     }
 }
