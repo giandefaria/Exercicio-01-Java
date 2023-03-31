80bba932e0c1b891ab0fed90814436c2fcafc1ad
Gianbdf <gian.defaria@hotmail.com>
2023-03-30 22:36:16 -0300

    arquivos iniciais exercicio 32

---
+++ Exercicio32Java/app/src/main/java/Exercicio32Java/App.java
@@ -0,0 +1,14 @@
+/*
+Escreva um algoritmo que leia três valores inteiros e verifique se eles podem ser os lados de um triângulo.
+Se forem, informar qual o tipo de triângulo que eles formam: equilátero, isóscele ou escaleno.
+Propriedade: o comprimento de cada lado de um triângulo é menor do que a soma dos comprimentos dos
+outros dois lados
+ */
+package Exercicio32Java;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
