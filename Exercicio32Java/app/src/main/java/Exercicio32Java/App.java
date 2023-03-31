b55237badefd110ec165c03e62bf411323919583
Gianbdf <gian.defaria@hotmail.com>
2023-03-31 00:24:19 -0300

    arquivos de entrada

--- Exercicio32Java/app/src/main/java/Exercicio32Java/App.java
+++ Exercicio32Java/app/src/main/java/Exercicio32Java/App.java
@@ -3,12 +3,23 @@ Escreva um algoritmo que leia três valores inteiros e verifique se eles podem s
 Se forem, informar qual o tipo de triângulo que eles formam: equilátero, isóscele ou escaleno.
 Propriedade: o comprimento de cada lado de um triângulo é menor do que a soma dos comprimentos dos
 outros dois lados
+
+a. Triângulo Equilátero: aquele que tem os comprimentos dos três lados iguais;
+b. Triângulo Isóscele: aquele que tem os comprimentos de dois lados iguais.
+c. Triângulo equilátero é também isóscele;
+d. Triângulo Escaleno: aquele que tem os comprimentos de seus três lados diferentes;
  */
 package Exercicio32Java;
+
+import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
+        
+        int ladoA = 0;
+        int ladoB = 0;
+        int ladoC = 0;
         
     }
 }
